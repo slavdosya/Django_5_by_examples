d66f63428dfc5e3eb5f514390ddfde51e6819ff1
vaceslavkaramysev <v.karamyshev@icloud.com>
2025-03-02 18:54:33 +0300

    Add template

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -3,6 +3,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -24,6 +29,9 @@ class Post(models.Model):
         choices=Status.choices,
         default=Status.DRAFT
     )
+
+    objects = models.Manager()
+    published = PublishManager()
 
     class Meta:
         ordering = ['-publish']
