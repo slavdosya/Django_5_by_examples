b1368bfa1e16fe84157eabecd5da157bdd5b940d
vaceslavkaramysev <v.karamyshev@icloud.com>
2025-03-31 14:40:03 +0300

    Add last page if page is empty

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 from blog.models import Post
 
@@ -8,8 +8,10 @@ def post_list(request):
     post_list = Post.published.all()
     paginator = Paginator(post_list, 3)
     page_number = request.GET.get('page', 1)
-    posts = paginator.page(page_number)
-
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
     return render(
         request,
         'blog/post/list.html',
